fa640815a9838df1b6642f56f926e80fc9352e92
Alexander Hughes <ahughes1231@gmail.com>
2015-05-03 13:05:54 -0700

    Add 'integer' requirement to superior_id migration, update show page to exclude superior if ID is nil

--- db/migrate/20150430231449_add_superior_id_to_public_entities.rb
+++ db/migrate/20150430231449_add_superior_id_to_public_entities.rb
@@ -1,5 +1,5 @@
 class AddSuperiorIdToPublicEntities < ActiveRecord::Migration
   def change
-    add_column :superior_id, :public_entities
+    add_column :superior_id, :public_entities, :integer
   end
 end
