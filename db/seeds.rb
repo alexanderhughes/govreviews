f3ac9200ea5705183d8fc76b4d30befde92bcf38
Nicholas Hughes <hughes.nick@gmail.com>
2015-07-14 17:24:22 -0700

    Fix google map functionality

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,8 +12,8 @@ c_statewide_officials = Category.find_or_create_by(name: 'Statewide Elected Offi
 #State Executive Offices
 pe = PublicEntity.create(name: "Office of the Governor of New York", authority_level: "state", address: "NYS State Capitol Building, State Street and Washington Ave, Albany, NY, 12224", description: "Governor of NYS", website: "https://www.governor.ny.gov", entity_type: "state executive")
 pe.categories.push(c, c_statewide_officials)
-cuomo = Chief.create(name: "Andrew M. Cuomo", title: "Governor of the State of New York", salary: "179,000", election:_info: "Elected at the general election held November 4, 2014, for a term of four years, expiring December 31 2018.")
-pe.push(cuomo)
+cuomo = Chief.create(name: "Andrew M. Cuomo", title: "Governor of the State of New York", salary: "179,000", election_info: "Elected at the general election held November 4, 2014, for a term of four years, expiring December 31 2018.")
+pe.chiefs.push(cuomo)
 
 #City Executive Offices
 pe = PublicEntity.create(name: "Mayor of New York City", authority_level: "city", address: "New York City Hall, City Hall Park, New York, NY, 10007", description: "Mayor of New York City", website: "http://www1.nyc.gov/office-of-the-mayor/", entity_type: "city executive")
