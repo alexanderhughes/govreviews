5dd0939fbc3270a69ac54c9f55acc79f6f4d3f81
Alexander Hughes <ahughes1231@gmail.com>
2015-11-01 11:15:26 -0500

    Fix category routes, css for categories on index page

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,18 +1,11 @@
 class CategoriesController < ApplicationController
   def index
     @categories = Category.all
-  end  
-
-  def new
-    @categories = Category.new
-  end
-  
-  def create
   end
   
   def show
-    @categories = Category.find(params[:id])
+    @category = Category.find(params[:id])
     @public_entities = PublicEntity.all
-  end    
+  end
   
 end
